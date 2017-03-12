a583f59acf651da4ce3dce6d0a9ea8ea63e04185
Otávio Pace <otaviopp8@gmail.com>
2017-03-12 11:21:17 -0300

    Configurações básicas em app.js

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const session = require("express-session");
+const passport = require("passport");
+const expressValidator = require("express-validator")
+const LocalStrategy = require("passport-local").Strategy;
+const multer = require("multer");
+const upload = multer({ dest: "./uploads" });
+const flash = require("connect-flash");
+const mongo = require("mongodb");
+const mongoose = require("mongoose");
+const db = mongoose.connection;
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +31,41 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Handle Sessions
+app.use(session({
+  secret: "secret",
+  saveUninitialized: true,
+  resave: true
+}));
+
+// passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
 app.use('/', index);
 app.use('/users', users);
