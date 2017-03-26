2e98533330f18462e9a539a30af373d67f625c19
Otávio Pace <otaviopp8@gmail.com>
2017-03-26 19:48:38 -0300

    Novos métodos adicionados ao UserSchema e correção na criação de usuário

--- models/user.js
+++ models/user.js
@@ -28,10 +28,25 @@ const User = mongoose.model("User", UserSchema)
 
 User.createUser = (newUser, callback) => {
     bcrypt.genSalt(10, (error, salt) => {
-        bcrypt.hash(newUser.passweord, salt, (error, hash) => {
+        bcrypt.hash(newUser.password, salt, (error, hash) => {
             newUser.password = hash
             newUser.save(callback)
         })
+    })
+}
+
+User.getUserById = (id, callback) => {
+    User.findById(id, callback)
+}
+
+User.getUserByUsername = (username, callback) => {
+    const query = { username }
+    User.findOne(query, callback)
+}
+
+User.comparePassword = (candidatePassword, hash, callback) => {
+    bcrypt.compare(candidatePassword, hash, (error, isMatch) => {
+        callback(null, isMatch)
     })
 }
 
