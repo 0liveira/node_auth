6e75285cbbcf8ae2299c2e2ec9606105cf8402e3
Otávio Pace <otaviopp8@gmail.com>
2017-03-26 19:49:20 -0300

    Configurações de passport adicionadas e login concluído

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 const multer = require("multer");
 const upload = multer({ dest: "./uploads" });
+const passport = require("passport")
+const LocalStrategy = require("passport-local").Strategy;
 
 const User = require("../models/user")
 
@@ -17,6 +19,39 @@ router.get("/register", function (req, res, next) {
 router.get("/login", function (req, res, next) {
   res.render("login", { title: "Login" })
 });
+
+router.post("/login", passport.authenticate("local", { failureRedirect: "/users/login", failureFlash: "Invalid username or password" }), function (req, res) {
+  req.flash("success", "You are now logged in")
+  res.redirect("/")
+})
+
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+  User.getUserById(id, (error, user) => {
+    done(error, user)
+  })
+})
+
+passport.use(new LocalStrategy((username, password, done) => {
+  User.getUserByUsername(username, (error, user) => {
+    if (error)
+      throw error
+    if (!user)
+      return done(null, false, { message: "Inexistent username" })
+    
+    User.comparePassword(password, user.password, (error, isMatch) => {
+      if (error)
+        return done(error)
+      if (isMatch)
+        return done(null, user)
+      else
+        return done(null, false, { message: "Invalid Password" })
+    })
+  })
+}))
 
 router.post("/register", upload.single("profileImage"), function (req, res, next) {
   let name = req.body.name
